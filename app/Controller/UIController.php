5e4e1d56694a6f3dedcd76ee55a7ee6b67a95458
varit.asawavetvutt@gmail.com <kiririn@kiririns-MacBook-Pro.local>
2014-10-31 15:37:42 +0700

    set framework,create controller and view

---
+++ app/Controller/UIController.php
@@ -0,0 +1,35 @@
+<?php
+//this is controller
+class UIController extends AppController {
+	//set layout
+	var $layout = 'cozy_home';
+	//set helper
+	public $helpers = array('Html','Form','Js','Text');
+	
+	public function index(){
+		//this is notting
+	}
+
+	public function Home(){
+		//this is home page	
+	}
+	public function AboutUs(){
+		//About Us Page
+	}
+	public function Contact(){
+		//Contact Page
+	}
+	public function Event(){
+		//Event Page
+	}
+	public function Product(){
+		//show product page
+	}
+	public function Search(){
+		//Search Product page
+	}
+
+}
+
+
+?>
